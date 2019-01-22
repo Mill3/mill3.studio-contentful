287a131b5c467576fd2374feb748efeaf64a427a
Antoine Girard <thetoine@gmail.com>
2019-01-22 13:43:12 -0500

    added all files

---
+++ gatsby-config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  siteMetadata: {
+    title: `MILL3 Studio`,
+    description: `Mill3 is a digital studio established in Montréal and specialized in strategy, design & web development.`,
+    author: `MILL3 Studio`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `9bzwr94keh5j`,
+        accessToken: `d6cc93293888bce59f3cf5d4f8bac85aff49df2212c3034cd673ef1c5756ef84`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locale`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-i18next`,
+      options: {
+        availableLngs: ['en', 'fr'],
+        fallbackLng: 'en',
+        debug: true,
+        // siteUrl: 'https://www.example.com/',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
