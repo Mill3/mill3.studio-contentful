1c92ef05aef7c38adfef9e5e3f73ea24c9ffe47b
Antoine Girard <thetoine@gmail.com>
2019-06-07 13:21:13 -0400

    testing preview server sided function

---
+++ src/pages/preview.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+
+import ProjectSingle from "@components/projects/ProjectSingle"
+
+class Preview extends Component {
+
+  static entryID = new URL(window.location.href).searchParams.get('entry')
+
+
+  constructor(props) {
+    super(props);
+    this.state = {  }
+    console.log('entryID:', new URL(window.location.href).searchParams.get('entry'))
+  }
+  render() {
+
+    const pageContext = {
+      locale: 'en'
+    }
+
+    return (
+      <div>
+        for previewing..
+        {/* <ProjectSingle pageContext={pageContext} /> */}
+      </div>
+    );
+  }
+}
+
+export default Preview;
