fc41e42c803760a0bbf6c2020ead22b8ab117f28
Dominic Mercier <dominic@dominic-mercier.com>
2019-06-17 10:04:00 -0400

    Next Project : name underline pas on hover - tout le temps

--- src/components/buttons/LinkButton.js
+++ src/components/buttons/LinkButton.js
@@ -1,6 +1,15 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { Box } from 'rebass'
+
+const HoverAnimation = keyframes`
+  0% {
+    transform: scaleX(0);
+  }
+  100% {
+    transform: scaleX(1);
+  }
+`
 
 export const LinkButtonStyle = styled.span`
   display: inline-block;
@@ -8,33 +17,27 @@ export const LinkButtonStyle = styled.span`
   font-family: ${props => props.theme.fonts.serif};
   color: #000;
   text-align: center;
-  transition: all 0.25s ease-in-out;
   font-weight: 300;
   position: relative;
-  span {
-    position: relative;
-    z-index: 2;
-    mix-blend-mode: multiply;
-  }
+
   &:after {
     content: "";
     display: block;
-    transition: all 0.25s ease-in-out;
     height: 3px;
-    width: 0;
     background-color: ${props => props.hoverColor ? props.hoverColor : `#000`};
     position: absolute;
     bottom: -8px;
     left: -10px;
+    right: -10px;
     z-index: 1;
+    transform-origin: top left;
   }
+
   &:hover {
     text-decoration: none;
-    /* color: ${props => props.hoverColor ? props.hoverColor : `#000`}; */
+
     &:after {
-      width: calc(100% + 20px);
-      /* height: calc(10px); */
-      /* background-color: ${props => props.hoverColor ? props.hoverColor : `#000`}; */
+      animation: ${HoverAnimation} 250ms ease-in-out 100ms both;
     }
   }
 `
@@ -42,7 +45,7 @@ export const LinkButtonStyle = styled.span`
 const LinkButton = ({children, hoverColor}) => {
   return (
     <Box as={LinkButtonStyle} fontSize={[6]} hoverColor={hoverColor}>
-      <span>{children}</span>{}
+      {children}
     </Box>
   );
 }
