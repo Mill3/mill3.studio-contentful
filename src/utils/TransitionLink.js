c36c6f15767bbf42886fb812b9b7ade05af83026
Antoine Girard <thetoine@gmail.com>
2019-06-18 16:33:59 -0400

    transition title and color

--- src/utils/TransitionLink.js
+++ src/utils/TransitionLink.js
@@ -2,14 +2,21 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { injectIntl } from 'react-intl'
 
+import LayoutContext from '@components/contexts/LayoutContext'
+
 const TransitionLinkComponent = ({ to, intl: { locale }, title, color, ...props }) => {
   const path = `/${locale}${to}`
 
   return (
-    <Link
-      {...props}
-      to={path}
-    />
+    <LayoutContext.Consumer>
+      {(data) => (
+        <Link
+          {...props}
+          to={path}
+          onMouseEnter={(e) => data.set({ transitionColor: color, transitionTitle: title })}
+        />
+      )}
+    </LayoutContext.Consumer>
   )
 }
 
