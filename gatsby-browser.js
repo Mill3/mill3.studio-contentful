1df80a7f4c6d25a73612d4265f65a48d514e09e5
Antoine Girard <thetoine@gmail.com>
2019-08-19 09:33:58 -0400

    nodejs based root redirect fallback

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,55 +10,14 @@ import { navigate } from 'gatsby'
 
 import './src/styles/fonts.css'
 
-// export const shouldUpdateScroll = () => {
-//   return false;
-// };
+export const onInitialClientRender = () => {
 
-// export const onInitialClientRender = () => {
+  let root = '/'
+  let detectedLocale = 'en'
 
-//   let root = '/'
+  // on root only, redirect
+  if (window.location.pathname === root) {
+    navigate(`${detectedLocale}/`)
+  }
 
-//   // TODO: let detectedLocale = detectLocale()
-//   let detectedLocale = 'en'
-
-//   // on root only, redirect
-//   if (window.location.pathname === root) {
-//     navigate(`${detectedLocale}/`)
-//   }
-
-// }
-
-// export const onPreRouteUpdate = ({ location, prevLocation }) => {
-//   console.log("Gatsby started to change location to", location.pathname)
-//   console.time('PreRouteUpdate');
-// }
-
-// export const onRouteUpdate = ({ location }) => {
-//   console.timeEnd('PreRouteUpdate');
-// }
-
-// export const shouldUpdateScroll = ({
-//   routerProps: { location },
-//   getSavedScrollPosition
-// }) => {
-//   const currentPosition = getSavedScrollPosition(location)
-//   console.log(currentPosition, getSavedScrollPosition, location)
-//   // console.log(SmoothScrollbar.getAll());
-
-//   // let scrollbar = SmoothScrollbar.getAll()[0]
-//   // // console.log(scrollbar);
-//   // if (scrollbar && currentPosition) {
-//   //   scrollbar.setPosition(currentPosition[0], currentPosition[1])
-//   // }
-
-
-//   // console.log('currentPosition:', currentPosition, location)
-//   // const queriedPosition = getSavedScrollPosition({ pathname: `/random` })
-
-//   setTimeout(() => {
-//     console.log(`should scroll up`)
-//     window.scrollTo(...(currentPosition || [0, 0]))
-//   }, TRANSITION_DURATION * 1000);
-
-//   return false
-// }
+}
