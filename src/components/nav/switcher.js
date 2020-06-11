cb9fe80908080211f0515e15f47772e94de9f895
Antoine Girard <thetoine@gmail.com>
2020-06-11 12:58:25 -0400

    language switch as regular <a> element

--- src/components/nav/switcher.js
+++ src/components/nav/switcher.js
@@ -3,30 +3,33 @@ import styled from 'styled-components'
 import { injectIntl } from 'react-intl'
 import { Text } from 'rebass'
 
-import TransitionLinkComponent from '@components/transitions/TransitionLink'
 import locales from '@locales/locales'
 
 const SwitcherLink = styled.a`
   display: ${props => props.hidden ? 'none' : 'inline-block'};
 `
 
-const Switcher = ({intl: { locale }, fontSizes }) => (
-  <>
-    {Object.keys(locales).map(key => (
-      <TransitionLinkComponent
-        key={key}
-        as={SwitcherLink}
-        hidden={key === locale ? true : false}
-        to={`/${locales[key].path}/`}
-        color={`#fff`}
-        localePrefix={false}
-      >
-        <Text fontSize={fontSizes}>
-          {locales[key].locale}
-        </Text>
-      </TransitionLinkComponent>
-    ))}
-  </>
-);
+const Switcher = (props) => {
+  const { intl: { locale }, fontSizes } = props
+
+  return(
+    <>
+      {Object.keys(locales).map(key => (
+        <a
+          key={key}
+          as={SwitcherLink}
+          hidden={key === locale ? true : false}
+          href={`/${locales[key].path}/`}
+          color={`#fff`}
+          localePrefix={false}
+        >
+          <Text fontSize={fontSizes}>
+            {locales[key].locale}
+          </Text>
+        </a>
+      ))}
+    </>
+  )
+};
 
 export default injectIntl(Switcher)
